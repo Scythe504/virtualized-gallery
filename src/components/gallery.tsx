eb867fb4703ae4bbf445337c23dbf290d1d6c569
Scythe504 <hellfireprince98@gmail.com>
2026-03-22 09:36:13 +0530

    stripped down useless components

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,14 +1,12 @@
-import { type Photo, type UseFetchPhotosResult } from "../hooks/useFetchPhotos"
+import { type UseFetchPhotosResult } from "../hooks/useFetchPhotos"
 import { ImageCard } from "./image-card"
 import Loader from "./loader"
 
 interface GalleryProps {
   pRes: UseFetchPhotosResult;
-  favouriteIds: Set<string>
-  handleToggle: (photo: Photo) => void;
 }
 
-export const Gallery = ({ pRes, favouriteIds, handleToggle }: GalleryProps) => {
+export const Gallery = ({ pRes }: GalleryProps) => {
 
   if (pRes.error) return <p>Failed to Load!</p>
   if (pRes.loading) return <div className="flex justify-center items-center h-full">
@@ -16,13 +14,11 @@ export const Gallery = ({ pRes, favouriteIds, handleToggle }: GalleryProps) => {
   </div>
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {pRes.photos.map((photo) => (
         <ImageCard
           key={photo.id}
           photo={photo}
-          favouriteIds={favouriteIds}
-          handleToggle={handleToggle}
         />
       ))}
     </div>
--- src/components/image-card.tsx
+++ src/components/image-card.tsx
@@ -1,22 +1,10 @@
 import type { Photo } from "../hooks/useFetchPhotos";
-import { Button } from "./button";
 
 interface ImageCardProps {
   photo: Photo;
-  favouriteIds: Set<string>;
-  handleToggle: (photo: Photo) => void;
 }
-export const HeartIcon = ({ fill }: { fill: boolean }) => <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org">
-  <path
-    d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
-    stroke="currentColor"
-    fill={fill ? "white" : "black"}
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  />
-</svg>
-export const ImageCard = ({ photo, favouriteIds, handleToggle }: ImageCardProps) => {
+
+export const ImageCard = ({ photo }: ImageCardProps) => {
 
   return <div className="rounded-sm flex flex-col relative overflow-clip aspect-square focus:shadow-md shadow-zinc-700 shadow-sm hover:scale-105 transition-transform duration-300 ease-in-out">
     {/* Header - By: <AUTHOR_NAME> */}
@@ -31,16 +19,6 @@ export const ImageCard = ({ photo, favouriteIds, handleToggle }: ImageCardProps)
         height={photo.height}
         width={photo.width}
       />
-    </div>
-    {/* Footer Favourites Button */}
-    <div className="absolute bottom-0 left-0 right-0 flex justify-end p-2">
-      <Button className="bg-black text-white"
-        onClick={() => handleToggle(photo)}
-      >
-        <HeartIcon
-          fill={favouriteIds.has(photo.id)}
-        />
-      </Button>
     </div>
   </div>
 }
